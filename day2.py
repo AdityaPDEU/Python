bb06324b4ef67e5d5e87a24ec0e7a38cf176496c
AdityaPDEU <shrivastavaaditya8797@gmail.com>
2025-01-23 15:59:59 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,109 @@
+# largest and smallest of the values
+''''def q1():
+    a=int(input(" enter a:"))
+    b=float(input(" enter b:"))
+    if a>b:
+        print("a:",a)
+    elif a<b:
+        print("b:",b)
+    else:
+        print(a,"=",b)
+
+q1()
+q1()
+q1()'''
+
+''''def q2():
+    a=float(input(" enter a:"))
+    b=float(input(" enter b:"))
+    c=int(input("enter c:"))
+    if a>b>c:
+        print("largest is a:",a)
+        print("b and c are smaller then a")
+    elif a<b>c:
+        print(" b is larger:",b)
+    else:
+        print("smaller is c :",c)
+q2()
+q2()
+q2()'''
+
+''''def q3():
+    
+    a=int(input(" enter a:"))
+    if a%2==0:
+        print("even")
+    else:
+        print("odd")
+q3()
+q3()'''
+
+''''def q4():
+    a=int(input(" enter a:"))
+    if a%10==0:
+        print(" the number is divisible")
+    else:
+        print("not divisible")
+q4()
+q4()'''
+
+'''def q5():
+    a=int(input(" enter age:"))
+    if a<18:
+        print("minior")
+    else:
+        print("major")
+q5()
+q5()'''
+
+'''def q6():
+    a=int(input(" enter number:"))'''
+
+'''def q7():
+    a=int(input(" enter year:"))
+    if a%4==0:
+        print("leap year")
+    else:
+        print("not leap year")
+q7()
+q7()'''
+
+'''def q8():
+    a=int(input(" enter first angle:"))
+    b=int(input(" enter second angle:"))
+    c=int(input(" enter third angle:"))
+    if a+b+c ==180:
+        print("trinalge is valid ")
+    else:
+        print(" not valid triangle please check angle")
+q8()
+q8()'''
+
+'''def q9():
+    a=int(input(" enter a:"))
+    if a>=0:
+        print(a)
+    else:
+        print(a*(-1))
+q9()
+q9()'''
+
+'''def q10():
+    a=int(input(" enter lenght:"))
+    b=float(input(" enter breadth:"))
+    c=2*(a+b)#perimeter
+    print(" perimeter",c)
+    d=a*b # area
+    print("area",d)
+    if d>c:
+        print(" area is greater ")
+    else:
+        print("nothing")
+q10()
+q10()'''
+
+    
+        
+
+
+
