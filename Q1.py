16266ce3f46674f1583a76e47d6d6b77012aeea4
AdityaPDEU <shrivastavaaditya8797@gmail.com>
2025-01-23 14:06:31 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,13 @@
+# largest and smallest of the values
+def q1():
+    a=int(input(" enter a:"))
+    b=float(input(" enter b:"))
+    if a>b:
+        print("a:",a)
+    elif a<b:
+        print("b:",b)
+    else:
+        print(a,"=",b)
+q1()
+q1()
+q1()
