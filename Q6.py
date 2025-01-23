92e1d3285db7e3832d84eab4beac3a0e0500debd
AdityaPDEU <shrivastavaaditya8797@gmail.com>
2025-01-23 14:24:06 +0530

    Add files via upload

---
+++ Q6.py
@@ -0,0 +1,9 @@
+# print the  digit of a given number
+def q6():
+    a=int(input(" enter number:"))
+    if(a<10):
+        print("digit:1")
+    elif(10>=a<100):
+        print("digit:2")
+q6()
+q6()
