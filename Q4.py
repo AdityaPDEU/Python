75e72db0cc8eb1dddf8d53b021b2fa0a19dda3e2
AdityaPDEU <shrivastavaaditya8797@gmail.com>
2025-01-23 14:15:03 +0530

    Add files via upload

---
+++ Q4.py
@@ -0,0 +1,9 @@
+# check given number is divisible by 10 or not
+def q4():
+    a=int(input(" enter a:"))
+    if a%10==0:
+        print(" the number is divisible")
+    else:
+        print("not divisible")
+q4()
+q4()
